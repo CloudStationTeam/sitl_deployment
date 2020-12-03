643d4c5117c9776dbb37252e3159318d4477bd8f
Chris Zhang <chizhang95@gmail.com>
2020-12-03 15:08:05 -0800

    added comments

--- start_sitl_fleet.sh
+++ start_sitl_fleet.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Configurables
-ARDUPILOT_LOC="../ardupilot/"
-CLOUDSTATION_IP="54.219.7.188"
-UDP_PORT=14550
+ARDUPILOT_LOC="../ardupilot/" # Location of ardupilot, either absolute path or relative to where this script is being executed from
+CLOUDSTATION_IP="54.219.7.188" # IP of CloudStation instance
+UDP_PORT=14550 # First UDP open port on CloudStation (will be incremented for each drone)
 
 
 echo "ArduPilot location: $ARDUPILOT_LOC"
