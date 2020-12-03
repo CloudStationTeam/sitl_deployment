3848d846366024405907657b1f6e2588631b8fa6
Chris Zhang <chizhang95@gmail.com>
2020-12-03 15:05:06 -0800

    switched to SITL supported enumeration scheme for drone instances

--- start_sitl_fleet.sh
+++ start_sitl_fleet.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Configurables
-ARDUPILOT_LOC="Github/ardupilot/"
+ARDUPILOT_LOC="../ardupilot/"
 CLOUDSTATION_IP="54.219.7.188"
 UDP_PORT=14550
 
@@ -17,15 +17,16 @@ echo "ArduPilot location: $ARDUPILOT_LOC"
 echo "# of rovers to start:"
 read rovers
 
+cd $ARDUPILOT_LOC
+
 # Launch Rovers
 echo "Launching $rovers rovers..."
-cd $ARDUPILOT_LOC
-cd Rover/
+
 for (( i=0; i<rovers; i++ ))
 do
 	echo "Assigning UDP port $UDP_PORT"
-	mintty -h always ../Tools/autotest/sim_vehicle.py --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
+	mintty Tools/autotest/sim_vehicle.py -v Rover --no-extra-ports -I $i --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
 	((UDP_PORT++))
-	sleep 8s
+	sleep 5s
 done
 
