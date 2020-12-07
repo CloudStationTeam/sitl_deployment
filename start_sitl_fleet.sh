b1ed096c9877e8305a18a39b2fcbda68ba9e9f0b
Chris Zhang <chizhang95@gmail.com>
2020-12-06 16:17:38 -0800

    added support for generating copters

--- start_sitl_fleet.sh
+++ start_sitl_fleet.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "Note: It is not recommended to generate multiple instances of a vehicle when running it for the first time, as it will need to be compiled."
 
 # Configurables
 ARDUPILOT_LOC="../ardupilot/" # Location of ardupilot, either absolute path or relative to where this script is being executed from
@@ -8,14 +9,16 @@ UDP_PORT=14550 # First UDP open port on CloudStation (will be incremented for ea
 
 echo "ArduPilot location: $ARDUPILOT_LOC"
 
-# echo "# of copters to start:"
-# read copters
+echo "# of copters to start:"
+read copters
 # echo "# of planes to start:"
 # read planes
 # echo "# of subs to start:"
 # read subs
 echo "# of rovers to start:"
 read rovers
+
+vehicle=0
 
 cd $ARDUPILOT_LOC
 
@@ -25,8 +28,20 @@ echo "Launching $rovers rovers..."
 for (( i=0; i<rovers; i++ ))
 do
 	echo "Assigning UDP port $UDP_PORT"
-	mintty Tools/autotest/sim_vehicle.py -v Rover --no-extra-ports -I $i --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
+	mintty Tools/autotest/sim_vehicle.py -v Rover --no-extra-ports -I $vehicle --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
 	((UDP_PORT++))
-	sleep 5s
+	((vehicle++))
+	sleep 8s
+done
+
+echo "Launching $copters copters..."
+
+for (( i=0; i<copters; i++ ))
+do
+	echo "Assigning UDP port $UDP_PORT"
+	mintty Tools/autotest/sim_vehicle.py -v ArduCopter --no-extra-ports -I $vehicle --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
+	((UDP_PORT++))
+	((vehicle++))
+	sleep 8s
 done
 
