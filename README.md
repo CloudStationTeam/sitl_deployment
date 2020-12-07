ad94c05d811cc71116d60b8deddfa54e3ea95b6c
Chris Zhang <chizhang95@gmail.com>
2020-12-06 20:33:08 -0800

    updated README and added command flags

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# sitl_deployment
+# SITL Fleet Auto-deployment
+
+This script automates the launching of SITL vehicles for testing purposes with CloudStation. It launches the desired numbers of each type of SITL vehicle, then appends the appropriate CloudStation UDP port its output list.
+
+## Prerequisites
+This script was developed and tested on a Windows 10 machine using a Cygwin terminal (which is required for SITL to run. Git Bash and other terminals, as far as we know, will not work). It has not been tested with Linux or other environments.
+
+It assumes that [SITL](https://ardupilot.org/dev/docs/SITL-setup-landingpage.html) has already been downloaded on the machine the script is being run on.
+
+## Launch Options
+	-h, --help 					Help message
+	--loc=/path/to/ardupilot 	Set the location of your ArduPilot directory
+	--ip=x.x.x.x 				Set the CloudStation IP
+	---udp=xxxxx 				Set the first open UDP port
+
+Location, IP, and UDP are required data for the program to run, although you can edit the start_sitl_fleet.sh script directly to change the default variables instead of using the flags.
+
+## Connecting to CloudStation
+After logging in to your CloudStation page, in the box labeled "Connect to Vehicle via ID," enter the UDP port bumber and hit "Connect" for each of the drones you created (they will begin with the UDP port you entered and increment by 1 for each vehicle).
--- start_sitl_fleet.sh
+++ start_sitl_fleet.sh
@@ -1,45 +1,86 @@
 #!/bin/sh
-echo "Note: It is not recommended to generate multiple instances of a vehicle when running it for the first time, as it will need to be compiled."
+
+HELP_MSG="
+	Usage:
+		-h, --help 			Help message
+		--loc=/path/to/ardupilot 	Set the location of your ArduPilot directory
+		--ip=x.x.x.x 			Set the CloudStation IP
+		---udp=xxxxx 			Set the first open UDP port
+	For more details see README"
 
 # Configurables
-ARDUPILOT_LOC="../ardupilot/" # Location of ardupilot, either absolute path or relative to where this script is being executed from
+ARDUPILOT_LOC="/path/to/ardupilot" # Location of ardupilot, either absolute path or relative to where this script is being executed from
 CLOUDSTATION_IP="54.219.7.188" # IP of CloudStation instance
 UDP_PORT=14550 # First UDP open port on CloudStation (will be incremented for each drone)
 
+# Command Line Args
+for arg in "$@"
+do
+    case $arg in
+        -h|--help)
+		echo "$HELP_MSG"
+        exit 0
+        ;;
+        --loc=*)
+        ARDUPILOT_LOC="${arg#*=}"
+        shift # Remove --cache= from processing
+        ;;
+        --ip=*)
+        CLOUDSTATION_IP="${arg#*=}"
+        shift # Remove --cache= from processing
+        ;;
+        --udp=*)
+        UDP_PORT="${arg#*=}"
+        shift # Remove --cache= from processing
+        ;;
+        *)
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
+echo "Note: It is not recommended to generate multiple instances of a vehicle when running it for the first time, as it will need to be compiled.
+"
 
 echo "ArduPilot location: $ARDUPILOT_LOC"
+echo "CloudStation IP: $CLOUDSTATION_IP"
+echo "First open UDP port: $UDP_PORT"
 
-echo "# of copters to start:"
+echo "
+# of copters to start:"
 read copters
 # echo "# of planes to start:"
 # read planes
 # echo "# of subs to start:"
 # read subs
-echo "# of rovers to start:"
+echo "
+# of rovers to start:"
 read rovers
 
 vehicle=0
 
 cd $ARDUPILOT_LOC
 
-# Launch Rovers
-echo "Launching $rovers rovers..."
-
-for (( i=0; i<rovers; i++ ))
-do
-	echo "Assigning UDP port $UDP_PORT"
-	mintty Tools/autotest/sim_vehicle.py -v Rover --no-extra-ports -I $vehicle --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
-	((UDP_PORT++))
-	((vehicle++))
-	sleep 8s
-done
-
-echo "Launching $copters copters..."
+echo "
+Launching $copters copters..."
 
 for (( i=0; i<copters; i++ ))
 do
 	echo "Assigning UDP port $UDP_PORT"
 	mintty Tools/autotest/sim_vehicle.py -v ArduCopter --no-extra-ports -I $vehicle --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
+	((UDP_PORT++))
+	((vehicle++))
+	sleep 8s
+done
+
+# Launch Rovers
+echo "
+Launching $rovers rovers..."
+
+for (( i=0; i<rovers; i++ ))
+do
+	echo "Assigning UDP port $UDP_PORT"
+	mintty Tools/autotest/sim_vehicle.py -v Rover --no-extra-ports -I $vehicle --out=udp:$CLOUDSTATION_IP:$UDP_PORT &
 	((UDP_PORT++))
 	((vehicle++))
 	sleep 8s
